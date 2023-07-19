c658b80208151b195d3a6075215d3c9f5d3cb479
mreza <mrezabmiri17@gmail.com>
2023-07-19 21:02:15 +0330

    router and Home Component Added to App Component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Suspense, lazy } from "react";
+
 import SideBar from "./components/SideBar";
+import Loading from "./components/Loading";
 
 const App = () => {
+  const Home = lazy(() => import("./pages/Home"));
   return (
-    <div className="content w-full h-screen">
-      <SideBar />
-    </div>
+    <Suspense fallback={<Loading />}>
+      <Router>
+        <div className="App">
+          <SideBar />
+          <div className="content">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/page2" element={<Home />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    </Suspense>
   );
 };
 
