42412788f15dc9fdd983902fba271d78e2077e5d
mreza <mrezabmiri17@gmail.com>
2023-07-19 11:42:36 +0330

    SideBar Component Built but not Completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import SideBar from "./components/SideBar";
+
 const App = () => {
-  return <div className="content w-full h-screen"></div>;
+  return (
+    <div className="content w-full h-screen">
+      <SideBar />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,7 @@
+const SideBar = () => {
+  return (
+    <div className="bg-green-500 fixed top-0 left-0 z-40 w-40 h-screen transition-transform -translate-x-full sm:translate-x-0"></div>
+  );
+};
+
+export default SideBar;
