d06e86f0de3378d7318acab4b024c58bc3f95b51
mreza <mrezabmiri17@gmail.com>
2023-07-21 03:28:43 +0330

    Completed Routing Procedure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Suspense, lazy } from "react";
-
 import SideBar from "./components/SideBar";
-import Loading from "./components/Loading";
-
 const App = () => {
-  const Home = lazy(() => import("./pages/Home"));
   return (
-    <Suspense fallback={<Loading />}>
-      <Router>
-        <div className="App">
-          <SideBar />
-          <div className="content">
-            <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route exact path="/page2" element={<Home />} />
-            </Routes>
-          </div>
-        </div>
-      </Router>
-    </Suspense>
+    <div className="App">
+      <SideBar />
+    </div>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import * as ReactDOM from "react-dom/client";
+
+import { Suspense, lazy } from "react";
+
+const App = lazy(() => import("./App"));
+const Home = lazy(() => import("./pages/Home"));
+const AddToTable = lazy(() => import("./pages/AddToTable"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <PageNotFound />, // this error should be rendered inside the <App />
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "add",
+        element: <AddToTable />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
