6061708056e510710b74fbc60fb018cac3089bee
mreza <mrezabmiri17@gmail.com>
2023-07-22 10:54:22 +0330

    Dynamic Error Page Replaced With  NotFound Page.

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,15 +2,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleLeft } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
-const PageNotFound = () => {
+const ErrorPage = (props) => {
+  const errorContentHandler = () => {
+    if (props.error == "404") return "Page Not Found";
+    return "I Don't Know";
+  };
   return (
     <div className="h-screen w-screen flex justify-center">
       <div className="w-full h-full z-50 bg-black text-white flex-col px-48 py-24 space-y-14">
         <div className="w-full text-6xl flex justify-center">
-          <h1 className=" ">Error 404 </h1>
+          <h1 className=" ">Error {props.error} </h1>
         </div>
         <div className="w-full text-6xl flex justify-center">
-          <p>Page Not Found</p>
+          <p>{errorContentHandler()}</p>
         </div>
         <div className="w-full text-6xl flex justify-center">
           <Link to="/" className="text-6xl block">
@@ -23,4 +27,4 @@ const PageNotFound = () => {
   );
 };
 
-export default PageNotFound;
+export default ErrorPage;
