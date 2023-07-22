2a768ea2bb3ab9f6e96353f867bc382b2418cda6
mreza <mrezabmiri17@gmail.com>
2023-07-22 11:59:36 +0330

    Edited Home and Table Component.

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,23 +1,23 @@
 import { useEffect } from "react";
 
 const Table = (props) => {
-  //   useEffect(() => {
-  //     tags += `<tr>
-  //                 <td>Name</td>
-  //                 <td>نام</td>
-  //                 <td>قیمت</td>
-  //                 <td>تغییرات</td>
-  //             </tr>`;
-  //   }, [props.table]);
-  let tags = "";
-
+  const renderHeader = () => {
+    return (
+      <tr>
+        <th className="w-1/5 px-6 py-4 float-left border-b-yellow-200">نام</th>
+        <th className="w-1/5 px-6 py-4 float-left border-b-yellow-200">قیمت</th>
+        <th className="w-1/5 px-6 py-4 float-left border-b-yellow-200">دسته</th>
+        <th className="w-1/5 px-6 py-4 float-left border-b-yellow-200">
+          امتیاز
+        </th>
+      </tr>
+    );
+  };
+  const renderBody = () => {};
   return (
-    <table>
-      <thead>
-        <td>نام</td>
-        <td>قیمت</td>
-        <td>تغییرات</td>
-      </thead>
+    <table className="min-w-full text-center text-sm font-light">
+      {renderHeader()}
+      {renderBody()}
     </table>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,21 @@
 import Table from "../components/Table";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleLeft } from "@fortawesome/free-solid-svg-icons";
+import { faCircleRight } from "@fortawesome/free-solid-svg-icons";
 
 function Home(props) {
   return (
     <div className="h-screen w-screen bg-gradient-to-l from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ... flex items-center justify-center">
-      <div className="h-3/4 w-8/12 bg-white rounded justify-center pl-10 ">
-        <Table table={props.table} />
+      <div className="h-3/4 w-8/12 bg-white rounded ">
+        <Table list={props.list} />
+        <div className="flex flex-row justify-center bg-green-700 w-full h-10">
+          <button className="h-full w-1/6 text-3xl text-white hover:text-pink-600">
+            <FontAwesomeIcon icon={faCircleLeft} />
+          </button>
+          <button className="h-full w-1/6 text-3xl text-white hover:text-pink-600 ">
+            <FontAwesomeIcon icon={faCircleRight} />
+          </button>
+        </div>
       </div>
     </div>
   );
