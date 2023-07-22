079ecf0518573f0fbf356eb10f34f09937077f7e
mreza <mrezabmiri17@gmail.com>
2023-07-22 09:05:33 +0330

    Routing Debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Loading from "./components/Loading";
 import SideBar from "./components/SideBar";
 import Home from "./pages/Home";
 import AddToTable from "./pages/AddToTable";
@@ -11,11 +10,17 @@ const App = () => {
     <Router>
       <div className="w-full h-screen">
         <SideBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/add" element={<AddToTable />} />
-          <Route path="/*" element={<PageNotFound />} />
-        </Routes>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/add">
+            <AddToTable />
+          </Route>
+          <Route path="*">
+            <PageNotFound />
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
