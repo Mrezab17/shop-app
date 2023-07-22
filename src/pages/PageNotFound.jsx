44dd7e52bd86f6a338c055b736eecb73151bde3e
mreza <mrezabmiri17@gmail.com>
2023-07-22 09:06:09 +0330

    Page Not Found Completed .

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -5,15 +5,15 @@ import { Link } from "react-router-dom";
 const PageNotFound = () => {
   return (
     <div className="h-screen w-screen flex justify-center">
-      <div className="w-full h-full z-50 bg-black text-white flex-col">
-        <div className="basis-1/3">
+      <div className="w-full h-full z-50 bg-black text-white flex-col px-48 py-24 space-y-14">
+        <div className="w-full text-6xl flex justify-center">
           <h1 className=" ">Error 404 </h1>
         </div>
-        <div className="basis-1/3">
+        <div className="w-full text-6xl flex justify-center">
           <p>Page Not Found</p>
         </div>
-        <div className="basis-1/3">
-          <Link to="/" className="text-8xl block">
+        <div className="w-full text-6xl flex justify-center">
+          <Link to="/" className="text-6xl block">
             <FontAwesomeIcon icon={faCircleLeft} />
             {"    "}Return Home
           </Link>
