f5bf772f776ee34a862dae005e016a7b981c7c57
mreza <mrezabmiri17@gmail.com>
2023-07-23 15:41:44 +0330

    Axios Added . Remove Handler Added .

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,65 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import { Suspense, lazy, useState, useEffect, useCallback } from "react";
+import axios from "axios";
 
 import SideBar from "./components/SideBar";
 import ErrorPage from "./pages/ErrorPage";
 import Loading from "./components/Loading";
 import Products from "./pages/Products";
 
+const baseURL = "https://json.xstack.ir/api/v1/products";
+
 const App = () => {
-  const [list, setList] = useState();
+  const [list, setList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState({});
 
-  const fetchProductsHandler = useCallback(async () => {
+  async function fetchDataHandler() {
     setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch("https://fakestoreapi.com/products");
-      if (!response.ok) {
-        throw new Error("Fetching Failed");
-      }
-      const data = await response.json();
-
-      const transformedList = data.map((data) => {
-        return {
-          id: data.id,
-          title: data.title,
-          price: data.price,
-          description: data.description,
-          category: data.category,
-          image: data.image,
-          rate: data.rating.rate,
-          rate_count: data.rating.count,
-        };
+    const response = await axios
+      .get(baseURL)
+      .then()
+      .catch((error) => {
+        setError(error);
       });
 
-      setList(transformedList);
-      setIsLoading(false);
-    } catch (error) {
-      setError(error.message);
-    }
-  });
-
+    setList(response.data.data);
+    setIsLoading(false);
+  }
   useEffect(() => {
-    fetchProductsHandler();
+    fetchDataHandler();
   }, []);
 
   const Home = lazy(() => import("./pages/Home"));
   const AddToTable = lazy(() => import("./pages/AddToTable"));
 
+  const removeHandler = (key) => {
+    const newList = list.filter((item) => item.id != key);
+    setList(newList);
+    console.log(newList);
+  };
+
   return (
     <>
-      {!isLoading ? (
+      {!isLoading && error ? (
         <Suspense fallback={<Loading />}>
           <Router>
             <div className="w-full h-screen">
               <SideBar />
               <Switch>
                 <Route exact path="/">
-                  <Home />
+                  <Redirect to="/products/1" />
                 </Route>
                 <Route exact path="/add">
                   <AddToTable />
                 </Route>
-                <Route exact path="/products/1">
-                  <Products list={list} />
+                <Route exact path="/products/:id">
+                  <Products list={list} onRemove={removeHandler} />
                 </Route>
                 <Route path="*">
                   <ErrorPage error={404} />
@@ -72,8 +68,10 @@ const App = () => {
             </div>
           </Router>
         </Suspense>
-      ) : (
+      ) : isLoading ? (
         <Loading />
+      ) : (
+        <p>{error}</p>
       )}
     </>
   );
